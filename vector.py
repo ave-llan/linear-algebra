4b1cba88410a588a66529870ea5fee2fe5be2a9f
John Leszczynski <jrleszczynski@gmail.com>
2016-06-12 17:34:59 -0400

    add basic vector class with add, subtract, scalar multiply

---
+++ vector.py
@@ -0,0 +1,39 @@
+class Vector(object):
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple(coordinates)
+            self.dimension = len(coordinates)
+
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
+
+
+    def __str__(self):
+        return 'Vector: {}'.format(self.coordinates)
+
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
+
+    def __add__(self, x):
+        self._validate_dimension(x)
+        return map(lambda a, b: a+b, self.coordinates, x.coordinates)
+
+    def __sub__(self, x):
+        self._validate_dimension(x)
+        return map(lambda a, b: a-b, self.coordinates, x.coordinates)
+
+    def __mul__(self, scalarInt):
+        return map(lambda a: a * scalarInt, self.coordinates)
+
+    def _validate_dimension(self, x):
+        if x.dimension != self.dimension:
+            raise ValueError('The vectors must have equal dimensinos')
+
+
+
